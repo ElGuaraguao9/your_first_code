10eeaba8d32f88ed42ff28e9e67116b8a6909e14
Emanuel Degró <168846852+ElGuaraguao9@users.noreply.github.com>
2024-05-11 23:21:37 -0400

    Update main.c
    "Hello, school" 32 times and print a message.

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("School\n");
+    printf("Hello, School\n");
   }
-  return 0;
+   char myText[] = "June";
+   int yeaR = 2024;
+  printf("I will start class in %s %d", myText, yeaR);
 }
